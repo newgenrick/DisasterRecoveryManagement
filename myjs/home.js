6fd7fe70e103850d91c5a8e91320891ffc59db73
newgenrick <rick.awas@gmail.com>
2018-09-27 00:02:59 +0530

    ui design

---
+++ myjs/home.js
@@ -0,0 +1,21 @@
+var bgImageArray = ["earthquake.jpg","indiaflood2.jpg"],
+base = "images/",
+secs = 4;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.body.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.body.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
+
